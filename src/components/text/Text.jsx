44e0c4eb7e92d81f4bd93257e7baa7f75a17982d
Jyophie <jyophie@gmail.com>
2023-11-04 16:22:00 +0900

    Design: 텍스트 시스템 적용

---
+++ src/components/text/Text.jsx
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const H2 = styled.h2`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 40px; /* 125% */
+`;
+
+export const H3 = styled.h3`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px; /* 125% */
+`;
+
+export const Body1Reg = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 125% */
+`;
+
+export const Body1Bol = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 125% */
+`;
+
+export const Body2Reg = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 133.333% */
+`;
+
+export const Body2Bol = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 133.333% */
+`;
+
+export const Body3Reg = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px; /* 137.5% */
+`;
+
+export const Body3Bol = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px; /* 137.5% */
+`;
+
+export const Caption1Reg = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px; /* 128.571% */
+`;
+
+export const Caption1Med = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px; /* 128.571% */
+`;
+
+export const Caption1Bol = styled.p`
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-family: Actor;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px; /* 128.571% */
+`;
