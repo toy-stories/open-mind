51577dd7e702a60e26f8b36389df667008ddd6ab
aowjarkwk <bulls1rose@naver.com>
2023-11-06 17:52:00 +0900

    임시

---
+++ src/components/dropdown/dropdown.style.jsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import COLORS from '../../utils/colors';
+import { ReactComponent as arrowUpIcon } from 'assets/icons/Arrow-up.svg';
+import { ReactComponent as arrowDownIcon } from 'assets/icons/Arrow-down.svg';
+
+export const DropdownContainer = styled.div`
+  width: 7.9rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+`;
+
+export const DropdownButton = styled.button`
+  word-break: keep-all;
+  display: flex;
+  padding: 0.8rem 1.2rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
+  border-radius: 0.8rem;
+  border: 1px solid ${COLORS.BLACK};
+  background-color: ${COLORS.WHITE};
+`;
+
+export const ArrowUpIcon = styled(arrowUpIcon)`
+  width: 1.4rem;
+  height: 1.4rem;
+`;
+export const ArrowDownIcon = styled(arrowDownIcon)`
+  width: 1.4rem;
+  height: 1.4rem;
+`;
+
+export const DropdownList = styled.ul`
+  display: flex;
+  width: 79px;
+  padding: 4px 0px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.8rem;
+  border: 1px solid ${COLORS.GRAY30};
+`;
+
+export const DropdownItem = styled.button`
+  display: flex;
+  padding: 0.6rem 1.6rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  background-color: ${COLORS.WHITE};
+`;
