8abe0693c1ce4e639e85e51b7c7266752a0e9f3f
Jyophie <jyophie@gmail.com>
2023-11-06 18:20:42 +0900

    Feat: 질문 작성하기 플로팅 버튼

---
+++ src/components/floatingButton/floatingButton.style.js
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+import COLORS from 'utils/colors.js';
+
+export const FloatingButton = styled.button`
+  display: flex;
+  width: 20.8rem;
+  height: 5.4rem;
+  padding: 1.2rem 2.4rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.BROWN40};
+  border-radius: 200px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -1,11 +1,12 @@
-import * as S from 'pages/post/PostPage.style.js';
+import * as S from 'pages/post/postPage.style.js';
 import { H2, Body1Bol } from 'components/text/Text.jsx';
 import headerImage from 'assets/images/header-background.png';
 import logo from 'assets/images/logo.png';
 import defaultProfileImage from 'assets/images/default-profile-image.png';
 import messagesIcon from 'assets/icons/Messages.svg';
 import emptyImg from 'assets/images/no-question.png';
-import ShareButtons from 'components/shareButtons/shareButtons';
+import ShareButtons from 'components/shareButtons/ShareButtons.jsx';
+import FloatingButton from 'components/floatingButton/FloatingButton.jsx';
 
 const PostPage = () => {
   return (
@@ -24,6 +25,7 @@ const PostPage = () => {
         </S.MessageContainer>
         <S.EmptyImage src={emptyImg} alt="빈 박스 이미지" />
       </S.FeedCardsContainer>
+      <FloatingButton />
     </S.PostPageContainer>
   );
 };
