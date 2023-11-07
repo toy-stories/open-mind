556cb9747d6cf83209ab9cd8b45da1bc5e460c59
fishwanders <yyvaca30@gmail.com>
2023-11-07 17:27:18 +0900

    Feat:기본  컴포넌트 제작

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,4 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as S from 'pages/main/mainPage.style.jsx';
+import LinkButton from 'components/linkButton/LinkButton.jsx';
+import logoImage from 'assets/images/logo.png';
+import mainBackgroundImage from 'assets/images/main-background.png';
+// import personImage from 'assets/icons/Person.svg';
+
 const MainPage = () => {
-  return <></>;
+  const [name, setName] = useState('');
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const id = '1';
+  const type = 'Q';
+  const isActive = true;
+
+  const answerPath = '/post/{id}/answer';
+  const LinkButtonPath = id ? '/list' : '/';
+
+  return (
+    <section>
+      <S.LogoImage src={logoImage} alt="로고이미지" />
+      <S.MainBackgroundImage src={mainBackgroundImage} alt="배경이미지" />
+      <input
+        type="text"
+        placeholder="이름을 입력하세요"
+        value={name}
+        onChange={handleInputChange}
+      />
+      <Link to={answerPath}>
+        <button disabled={!name}>질문 받기</button>
+      </Link>
+      <Link to={LinkButtonPath}>
+        <LinkButton type={type} isActive={isActive} />
+      </Link>
+    </section>
+  );
 };
 export default MainPage;
