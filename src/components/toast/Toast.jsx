f8563c48a523a231c0a6a8df44055fc5a832aaff
Jyophie <jyophie@gmail.com>
2023-11-07 16:43:58 +0900

    Feat: 메세지 토스트 추가

---
+++ src/components/toast/Toast.jsx
@@ -0,0 +1,12 @@
+import * as S from 'components/toast/toast.style.js';
+import { Caption1Med } from 'components/text/Text.jsx';
+
+const Toast = () => {
+  return (
+    <S.ToastBox>
+      <Caption1Med>URL이 복사되었습니다.</Caption1Med>
+    </S.ToastBox>
+  );
+};
+
+export default Toast;
