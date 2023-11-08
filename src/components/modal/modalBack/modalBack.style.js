4b5609b89c35d7dca50d249d5455de04648264eb
Hayden <82023300+HaydenDevK@users.noreply.github.com>
2023-11-08 15:13:16 +0900

    Feat: 모달 기본 구조 구현 (#6)

---
+++ src/components/modal/modalBack/modalBack.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ModalBackWrapper = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
