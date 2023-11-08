92bc09574a879cb69696832cc2bd3fda0d16fa7e
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2023-11-08 15:09:30 +0900

    Feat:기본  컴포넌트 제작 (#4)

---
+++ src/pages/main/mainPage.style.jsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+import COLORS from 'utils/colors.js';
+
+export const LogoImage = styled.img`
+  width: 456px;
+  height: 180px;
+`;
+
+export const MainBackgroundImage = styled.img`
+  width: 1200px;
+  height: 627px;
+`;
