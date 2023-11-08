4e3a061fba774ff801470f2ec576bf415f154e9d
Jyophie <jyophie@gmail.com>
2023-11-08 11:22:34 +0900

    Feat: 질문 작성하기 플로팅 버튼 모바일 문구 변경

--- src/components/floatingButton/FloatingButton.jsx
+++ src/components/floatingButton/FloatingButton.jsx
@@ -1,10 +1,36 @@
 import * as S from 'components/floatingButton/floatingButton.style.jsx';
+import { useState, useEffect } from 'react';
 import { Body1Bol } from 'components/text/Text.jsx';
+import { RESPONSIBLE_SIZE } from 'utils/constants.js';
 
 const FloatingButton = () => {
+  const [isMobileSize, setIsMobileSize] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mediaQuery = window.matchMedia(
+        `(max-width: ${RESPONSIBLE_SIZE.mobile})`,
+      );
+      setIsMobileSize(mediaQuery.matches);
+    };
+
+    handleResize();
+
+    const mediaQuery = window.matchMedia(
+      `(max-width: ${RESPONSIBLE_SIZE.mobile})`,
+    );
+    mediaQuery.addListener(handleResize);
+
+    return () => mediaQuery.removeListener(handleResize);
+  }, []);
+
   return (
     <S.FloatingButton>
-      <Body1Bol>질문 작성하기</Body1Bol>
+      {isMobileSize ? (
+        <Body1Bol>질문 작성</Body1Bol>
+      ) : (
+        <Body1Bol>질문 작성하기</Body1Bol>
+      )}
     </S.FloatingButton>
   );
 };
