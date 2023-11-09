e975bcd81f5afc1b070e2d34a51a7722a53d67c8
Jyophie <jyophie@gmail.com>
2023-11-09 15:52:47 +0900

    Fix: 충돌 해결

--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -6,12 +6,17 @@ import defaultProfileImage from 'assets/images/default-profile-image.png';
 import emptyImg from 'assets/images/no-question.png';
 import ShareButtons from 'components/shareButtons/ShareButtons.jsx';
 import FloatingButton from 'components/floatingButton/FloatingButton.jsx';
+import PostCardList from 'components/postCards/PostCardList';
 // import EditButton from 'components/editButton/EditButton';
 
 // 테스트 코드
 // const isActive = true;
 
 const PostPage = () => {
+  // 추후 api연동하여 데이터 프롭스로 받기
+  // 테스트코드
+  const hasQuestion = true;
+
   return (
     <S.PostPageContainer>
       <S.HeaderImage src={headerImage} alt="헤더 배경 이미지" />
@@ -21,13 +26,17 @@ const PostPage = () => {
         <H2>아초는 고양이</H2>
       </S.HeaderUserProfile>
       <ShareButtons />
-      <S.FeedCardsBox>
-        <S.MessageBox>
-          <S.MessageIcon />
-          <Body1Bol>아직 질문이 없습니다.</Body1Bol>
-        </S.MessageBox>
-        <S.EmptyImage src={emptyImg} alt="빈 박스 이미지" />
-      </S.FeedCardsBox>
+      {hasQuestion ? (
+        <PostCardList />
+      ) : (
+        <S.FeedCardsBox>
+          <S.MessageBox>
+            <S.MessageIcon alt="메세지 아이콘" />
+            <Body1Bol>아직 질문이 없습니다.</Body1Bol>
+          </S.MessageBox>
+          <S.EmptyImage src={emptyImg} alt="빈 박스 이미지" />
+        </S.FeedCardsBox>
+      )}
       <FloatingButton type="W" />
       {/* <EditButton isActive={isActive} /> */}
     </S.PostPageContainer>
