845e26f574fc1807d1b412abc66c1c7148eeb22c
Jyophie <jyophie@gmail.com>
2023-11-09 17:13:40 +0900

    Fix: 명칭 수정

--- src/components/qnaForm/QnaForm.jsx
+++ src/components/qnaForm/QnaForm.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import * as S from 'components/cardAnswerInput/cardAnswerInput.style.jsx';
+import * as S from 'components/qnaForm/qnaForm.style.jsx';
 import { textType } from 'components/text/Text.jsx';
 
 const CardAnswerInput = () => {
-  const [answer, setAnswer] = useState('');
+  const [input, setInput] = useState('');
   const handleInputChange = (event) => {
-    setAnswer(event.target.value);
+    setInput(event.target.value);
   };
 
   return (
     <S.AnswerBox>
       <S.AnswerInput
         placeholder="답변을 입력해주세요"
-        value={answer}
+        value={input}
         onChange={handleInputChange}
       />
-      <S.StyledAnswerButton answer={answer}>
+      <S.StyledAnswerButton answer={input}>
         <S.Text type={textType.Body3Reg} text="답변 완료" />
       </S.StyledAnswerButton>
     </S.AnswerBox>
