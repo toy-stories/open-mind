fc9dadfecdf1ead84198ae72e5370167584d244b
HaydenDevK <hayden.dev.k@gmail.com>
2023-11-11 19:03:31 +0900

    Refactor: QnaForm 확장성 개선 등

--- src/components/qnaForm/qnaForm.style.jsx
+++ src/components/qnaForm/qnaForm.style.jsx
@@ -1,25 +1,30 @@
 import styled, { css } from 'styled-components';
 import COLORS from 'utils/colors';
 
-export const AnswerBox = styled.form`
+export const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
-export const AnswerInput = styled.input`
-  display: flex;
-  text-align: start;
+export const Textarea = styled.textarea`
   width: 100%;
   height: 18.6rem;
   margin-top: 0.4rem;
   margin-bottom: 0.8rem;
   padding: 1.6rem;
+  border: 1px solid transparent;
   border-radius: 8px;
   background: ${COLORS.GRAY20};
   color: ${COLORS.GRAY40};
+  resize: none;
+  outline: none;
+
+  &:focus {
+    border: 1px solid ${COLORS.BROWN40};
+  }
 `;
 
-export const StyledAnswerButton = styled.button`
+export const Button = styled.button`
   display: flex;
   width: 100%;
   height: 4.6rem;
