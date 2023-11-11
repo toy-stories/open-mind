aef58e9989cc150e4cd5215385f4b0ace3e88008
HaydenDevK <hayden.dev.k@gmail.com>
2023-11-11 19:06:22 +0900

    Feat: QuestionModal 세부 구현

--- src/components/modal/modalContent/QuestionModal.jsx
+++ src/components/modal/modalContent/QuestionModal.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import useAsync from 'hooks/useAsync.js';
 import * as S from './questionModal.style.js';
 import createQuestion from 'utils/createQuestion.js';
+import { TextType, Text } from 'components/text/Text.jsx';
+import COLORS from 'utils/colors.js';
+import QnaForm from 'components/qnaForm/QnaForm.jsx';
 
 const QuestionModal = ({ onClickClose }) => {
+  const [input, setInput] = useState('');
+
+  const [isLoading, error, createQuestionAsync] = useAsync(createQuestion);
+
+  const subjectId = '158';
+  const content = '무슨 강아지를 가장 좋아하시나요?';
+  // TODO 토스트 띄우고 창 닫기
+
+  async function handleCreateQuestion() {
+    const result = await createQuestionAsync(subjectId, content);
+    console.log(result);
+    // TODO 토스트 띄우고 창 닫기
+  }
+
   return (
     <S.QuestionModalWrapper
       onClick={(e) => {
@@ -11,16 +29,29 @@ const QuestionModal = ({ onClickClose }) => {
     >
       <S.TitleContainer>
         <S.TitleImage width="28px" height="28px" />
-        <S.TitleText>질문을 작성하세요</S.TitleText>
+        <Text
+          $normalType={TextType.H3}
+          text="질문을 작성하세요"
+          style={{ color: COLORS.BLACK }}
+        />
         <S.CloseButton onClick={onClickClose}>
           <S.CloseImage width="28px" height="28px" />
         </S.CloseButton>
       </S.TitleContainer>
       <S.SubjectContainer>
-        <S.To>To.</S.To>
+        <Text $normalType={TextType.Body2Bol} style={{ marginRight: '0.3rem' }}>
+          To.
+        </Text>
         <S.SubjectImage src="" width="28px" height="28px" />
-        <S.SubjectName>고양이가 어쩌구</S.SubjectName>
+        <Text $normalType={TextType.Body3Reg}>고양이가 어쩌구</Text>
       </S.SubjectContainer>
+      <QnaForm
+        input={input}
+        handleInputChange={(e) => setInput(e.target.value)}
+        inputPlaceholder="질문을 입력해주세요"
+        buttonText="질문 보내기"
+        onClickButton={handleCreateQuestion}
+      />
     </S.QuestionModalWrapper>
   );
 };
--- src/components/modal/modalContent/questionModal.style.js
+++ src/components/modal/modalContent/questionModal.style.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import COLORS from 'utils/colors.js';
-import { H3, Body2Bol, Body3Reg } from 'components/text/Text.jsx';
 import { ReactComponent as MessageIcon } from 'assets/icons/Messages.svg';
 import { ReactComponent as CloseIcon } from 'assets/icons/Close.svg';
 
@@ -22,8 +21,6 @@ export const TitleContainer = styled.div`
 
 export const TitleImage = styled(MessageIcon)``;
 
-export const TitleText = styled(H3)``;
-
 export const CloseButton = styled.button``;
 
 export const SubjectContainer = styled.div`
@@ -33,12 +30,6 @@ export const SubjectContainer = styled.div`
 
 export const CloseImage = styled(CloseIcon)``;
 
-export const To = styled(Body2Bol)`
-  margin-right: 0.3rem;
-`;
-
 export const SubjectImage = styled.img`
   margin-right: 0.4rem;
 `;
-
-export const SubjectName = styled(Body3Reg)``;
