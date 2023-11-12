2e0284c96e34494ac6af4e7ae2772b157f05d117
HaydenDevK <hayden.dev.k@gmail.com>
2023-11-12 10:46:00 +0900

    Refactor: Toast 확장성 개선

--- src/components/toast/Toast.jsx
+++ src/components/toast/Toast.jsx
@@ -1,10 +1,10 @@
 import * as S from 'components/toast/toast.style.jsx';
 import { Text, TextType } from 'components/text/Text.jsx';
 
-const Toast = () => {
+const Toast = ({ text }) => {
   return (
     <S.ToastBox>
-      <Text $normalType={TextType.Caption1Med} text="URL이 복사되었습니다." />
+      <Text $normalType={TextType.Caption1Med} text={text} />
     </S.ToastBox>
   );
 };
