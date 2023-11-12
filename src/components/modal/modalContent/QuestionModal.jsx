49a731b1aae509a5556cb9cc4124dbf6dbf1537c
HaydenDevK <hayden.dev.k@gmail.com>
2023-11-12 20:01:52 +0900

    Feat: QuestionModal 토스트 구현

--- src/components/modal/modalContent/QuestionModal.jsx
+++ src/components/modal/modalContent/QuestionModal.jsx
@@ -1,25 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useAsync from 'hooks/useAsync.js';
 import * as S from './questionModal.style.js';
 import createQuestion from 'utils/createQuestion.js';
 import { TextType, Text } from 'components/text/Text.jsx';
 import COLORS from 'utils/colors.js';
 import QnaForm from 'components/qnaForm/QnaForm.jsx';
+import Toast from 'components/toast/Toast.jsx';
+
+const sampleId = '158';
+
+const TOAST_TEXT_TYPE = {
+  NONE: '',
+  SUCCESS: '질문이 정상 등록되었습니다',
+  LOADING: '질문을 등록 중입니다',
+  ERROR: '오류가 발생했습니다',
+  EMPTY: '질문 내용을 입력해주세요.',
+};
 
 const QuestionModal = ({ onClickClose }) => {
-  const [input, setInput] = useState('');
-
-  const [isLoading, error, createQuestionAsync] = useAsync(createQuestion);
-
-  const subjectId = '158';
-  const content = '무슨 강아지를 가장 좋아하시나요?';
-  // TODO 토스트 띄우고 창 닫기
+  const [question, setQuestion] = useState('');
+  const [toastStatus, setToastStatus] = useState('NONE');
+  const [isPending, error, createQuestionAsync] = useAsync(createQuestion);
 
   async function handleCreateQuestion() {
-    const result = await createQuestionAsync(subjectId, content);
-    console.log(result);
-    // TODO 토스트 띄우고 창 닫기
+    if (!question) {
+      setToastStatus('EMPTY');
+      return;
+    }
+
+    if (isPending) {
+      setToastStatus('LOADING');
+      return;
+    }
+
+    const result = await createQuestionAsync({
+      subjectId: sampleId,
+      content: question,
+    });
+
+    if (result) {
+      setToastStatus('SUCCESS');
+      setQuestion('');
+    } else {
+      setToastStatus('ERROR');
+    }
   }
+
+  useEffect(() => {
+    let timer;
+    if (toastStatus !== 'NONE') {
+      timer = setTimeout(() => {
+        setToastStatus('NONE');
+      }, 5000);
+    }
+    return () => clearTimeout(timer);
+  }, [toastStatus]);
 
   return (
     <S.QuestionModalWrapper
@@ -47,12 +82,13 @@ const QuestionModal = ({ onClickClose }) => {
         <Text $normalType={TextType.Body3Reg}>고양이가 어쩌구</Text>
       </S.SubjectContainer>
       <QnaForm
-        input={input}
-        handleInputChange={(e) => setInput(e.target.value)}
+        input={question}
+        handleInputChange={(e) => setQuestion(e.target.value)}
         inputPlaceholder="질문을 입력해주세요"
         buttonText="질문 보내기"
         onClickButton={handleCreateQuestion}
       />
+      {toastStatus !== 'NONE' && <Toast text={TOAST_TEXT_TYPE[toastStatus]} />}
     </S.QuestionModalWrapper>
   );
 };
