5b6daed908d10d11d4b6c93d1cb8412f7dfc0cb3
이승연 <seung-yeon@iseung-yeon-ui-MacBookAir.local>
2023-11-14 13:37:02 +0900

    Refactor: 삭제하기 버튼 컨펌창 라이브러리 적용(sweetalert2)

--- src/components/answerCards/AnswerCardList.jsx
+++ src/components/answerCards/AnswerCardList.jsx
@@ -6,7 +6,7 @@ import { postCreateReaction } from 'pages/post/postPage.js';
 import { useState } from 'react';
 import useAsync from 'hooks/useAsync.js';
 import { fetchClient } from 'utils/apiClient';
-import { CONFIRM_MESSAGE, DELETE_USER_MESSAGE } from 'utils/constants.js';
+import Swal from 'sweetalert2';
 
 const REACTION_MAX_INT = 2147483647;
 
@@ -39,16 +39,39 @@ const AnswerCardList = ({ questionInfo, subjectOwner, subjectId }) => {
   };
 
   const handleDeleteId = async () => {
-    if (window.confirm(CONFIRM_MESSAGE)) {
-      await fetchClient({
-        url: `subjects/${subjectId}/`,
-        method: 'DELETE',
-      });
-      localStorage.removeItem('userId');
-      alert(DELETE_USER_MESSAGE);
-      window.location.replace('/');
-    }
+    Swal.fire({
+      title:
+        '[주의] 계정 삭제는 복구할 수 없으며, 모든 데이터가 영구적으로 제거됩니다.',
+      text: '계속하시겠습니까?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '계정삭제',
+      cancelButtonText: '취소',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetchClient({
+          url: `subjects/${subjectId}/`,
+          method: 'DELETE',
+        }).then((result) => {
+          localStorage.removeItem('userId');
+          Swal.fire({
+            title: 'Deleted!',
+            text: '계정이 삭제되었습니다. 메인페이지로 돌아갑니다.',
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: '확인',
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.replace('/');
+            }
+          });
+        });
+      }
+    });
   };
+
   return (
     <>
       <FloatingButton type="A" onClick={handleDeleteId} />
