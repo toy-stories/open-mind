d466d2421f40b61037ab9d07b13f971a4c6f5a2c
orrhrr <120437902+orrhrr@users.noreply.github.com>
2023-11-14 17:47:32 +0900

    Refactor: 답변 거절 상태일 때 '답변 삭제'버튼을 '답변 받기'로 바꾸기 (#32)
    Co-authored-by: 이승연 <seung-yeon@iseung-yeon-ui-MacBookAir.local>

--- src/components/kebabButton/KebabMenuButtons.jsx
+++ src/components/kebabButton/KebabMenuButtons.jsx
@@ -1,4 +1,5 @@
 import * as S from 'components/kebabButton/kebabButton.style.jsx';
+import { Text, TextType } from 'components/text/Text';
 
 const KebabMenuButtons = ({
   onRefuseAnswerClick,
@@ -13,16 +14,19 @@ const KebabMenuButtons = ({
     <S.KebabMenuList>
       {!isRejectedAnswer && !hasAnswer && (
         <S.KebabMenuButton onClick={onRefuseAnswerClick}>
-          답변 거절
+          <Text $normalType={TextType.Caption1Med} text="답변 거절" />
         </S.KebabMenuButton>
       )}
       {hasAnswer && (
         <S.KebabMenuButton onClick={onDeleteAnswerClick}>
-          답변 삭제
+          <Text
+            $normalType={TextType.Caption1Med}
+            text={isRejectedAnswer ? '답변 받기' : '답변 삭제'}
+          />
         </S.KebabMenuButton>
       )}
       <S.KebabMenuButton onClick={onDeleteQuestionClick}>
-        질문 삭제
+        <Text $normalType={TextType.Caption1Med} text="질문 삭제" />
       </S.KebabMenuButton>
     </S.KebabMenuList>
   );
