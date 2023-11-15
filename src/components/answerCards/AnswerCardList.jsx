562014e66326e86418d38dcbccc816d769bba091
이승연 <seung-yeon@iseung-yeon-ui-MacBookAir.local>
2023-11-15 11:36:38 +0900

    Refactor: deleteUser 모듈추가, 삭제하기 버튼 질문없을 때도 뜨게 수정

--- src/components/answerCards/AnswerCardList.jsx
+++ src/components/answerCards/AnswerCardList.jsx
@@ -5,8 +5,7 @@ import FloatingButton from 'components/floatingButton/FloatingButton.jsx';
 import { postCreateReaction } from 'pages/post/postPage.js';
 import useAsync from 'hooks/useAsync.js';
 import LoadingSpinner from 'components/tempLoading/TempLoading';
-import { fetchClient } from 'utils/apiClient';
-import Swal from 'sweetalert2';
+import { handleDeleteUser } from 'utils/deleteUser';
 
 const REACTION_MAX_INT = 2147483647;
 
@@ -40,43 +39,13 @@ const AnswerCardList = ({
     localStorage.setItem(type, JSON.stringify(localStorageReaction));
   };
 
-  const handleDeleteId = async () => {
-    Swal.fire({
-      title:
-        '[주의] 계정 삭제는 복구할 수 없으며, 모든 데이터가 영구적으로 제거됩니다.',
-      text: '계속하시겠습니까?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: '계정삭제',
-      cancelButtonText: '취소',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetchClient({
-          url: `subjects/${subjectOwner.id}/`,
-          method: 'DELETE',
-        }).then((result) => {
-          localStorage.removeItem('userId');
-          Swal.fire({
-            title: '계정삭제완료',
-            text: '계정이 삭제되었습니다. 메인페이지로 돌아갑니다.',
-            icon: 'success',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: '확인',
-          }).then((result) => {
-            if (result.isConfirmed) {
-              window.location.replace('/');
-            }
-          });
-        });
-      }
-    });
+  const handleDeleteClick = () => {
+    handleDeleteUser(subjectOwner);
   };
 
   return (
     <>
-      <FloatingButton type="A" onClick={handleDeleteId} />
+      <FloatingButton type="A" onClick={handleDeleteClick} />
       <S.PostCardList>
         <S.PostCardListTitleBox>
           <S.SpeechBubble />
