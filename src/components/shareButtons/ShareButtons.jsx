2bb450b73dd97e76179dfffd06007e5379ed7d65
aowjarkwk <bulls1rose@naver.com>
2023-11-15 18:45:10 +0900

    Feat: 페이스북 공유하기 기능

--- src/components/shareButtons/ShareButtons.jsx
+++ src/components/shareButtons/ShareButtons.jsx
@@ -5,7 +5,7 @@ import shareKakaoIcon from 'assets/icons/share-kakao.png';
 import shareFacebookIcon from 'assets/icons/share-facebook.png';
 import Toast from 'components/toast/Toast.jsx';
 
-const ShareButtons = () => {
+const ShareButtons = ({ subjectOwner }) => {
   const [showToast, setShowToast] = useState(false);
 
   const copyUrlToClipboard = () => {
@@ -19,6 +19,9 @@ const ShareButtons = () => {
         console.error('클립보드 복사에 실패했습니다.', err);
       });
   };
+  const shareLink = `${window.location.origin}/post/${subjectOwner.id}`;
+  const handleFacebookClick = () =>
+    window.open(`http://www.facebook.com/sharer.php?u=${shareLink}`);
 
   useEffect(() => {
     let timer;
@@ -38,7 +41,11 @@ const ShareButtons = () => {
         onClick={copyUrlToClipboard}
       />
       <S.ShareButton src={shareKakaoIcon} alt="카카오 공유하기" />
-      <S.ShareButton src={shareFacebookIcon} alt="페이스북 공유하기" />
+      <S.ShareButton
+        src={shareFacebookIcon}
+        alt="페이스북 공유하기"
+        onClick={handleFacebookClick}
+      />
       {showToast && <Toast text="URL이 복사되었습니다." />}
     </S.ShareButtons>
   );
